fef72ac9f0ee5988556ef434e9d55d2b88da2fd4
kandarpa.anuradha <kandarpa.anuradha@tri03lap-085f2.trianz.int>
2019-05-28 14:48:19 +0530

    #commenting test files

--- src/test/java/com/trianz/watch_dog/backup/CreateBackupFileTest.java
+++ src/test/java/com/trianz/watch_dog/backup/CreateBackupFileTest.java
@@ -1,20 +1,20 @@
-/**
- * 
- */
-package com.trianz.watch_dog.backup;
-
-import java.io.File;
-import java.io.IOException;
-
-import org.junit.Test;
-
-/**
- * @author Mahendhar.Nallabolu
- *
- */
-public class CreateBackupFileTest {
-	@Test
-	void createBackup(String filename, File destfilepath) throws IOException {
-
-	}
-}
+///**
+// * 
+// */
+//package com.trianz.watch_dog.backup;
+//
+//import java.io.File;
+//import java.io.IOException;
+//
+//import org.junit.Test;
+//
+///**
+// * @author Mahendhar.Nallabolu
+// *
+// */
+//public class CreateBackupFileTest {
+//	@Test
+//	void createBackup(String filename, File destfilepath) throws IOException {
+//
+//	}
+//}
--- src/test/java/com/trianz/watch_dog/watchfile/WatchFileServiceTest.java
+++ src/test/java/com/trianz/watch_dog/watchfile/WatchFileServiceTest.java
@@ -1,26 +1,26 @@
-package com.trianz.watch_dog.watchfile;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class WatchFileServiceTest {
-	@Test
-	void callRegisterDirectories(final Path start) throws IOException {
-
-	}
-
-	@Test
-	void registerDirectory(Path dir) throws IOException {
-
-	}
-
-	@Test
-	void processEvents() throws IOException {
-
-	}
-}
+//package com.trianz.watch_dog.watchfile;
+//
+//import java.io.IOException;
+//import java.nio.file.Path;
+//
+//import org.junit.Test;
+//
+///**
+// * Unit test for simple App.
+// */
+//public class WatchFileServiceTest {
+//	@Test
+//	void callRegisterDirectories(final Path start) throws IOException {
+//
+//	}
+//
+//	@Test
+//	void registerDirectory(Path dir) throws IOException {
+//
+//	}
+//
+//	@Test
+//	void processEvents() throws IOException {
+//
+//	}
+//}
